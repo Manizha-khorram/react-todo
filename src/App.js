022c167c5cba0bc403c0343f7bdc4c30687eb6cb
Manizha-khorram <108601629+Manizha-khorram@users.noreply.github.com>
2023-06-02 01:54:50 -0400

    Adding to do list for lesson_1_1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React from 'react'
 
+const todoList = [
+    {
+        id: 1,
+        title: 'Complete assignment',
+    },
+    {
+        id: 2,
+        title: 'Play with kids',
+    },
+    {
+        id: 3,
+        title: 'Buy some groceries',
+    },
+]
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <h1>Todo List</h1>
+            </header>
+            <hr />
+
+            <ul>
+                {todoList.map((x) => (
+                    <li key={x.id}>{x.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
