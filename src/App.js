313698929a15b100c34d343b9967e52c59706afe
Manizha-khorram <108601629+Manizha-khorram@users.noreply.github.com>
2023-06-25 06:19:47 -0400

    Lifting state, props handeling and controlled component.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
 function App() {
-    const [newTodo, setnewTodo] = useState('')
+    //State variables for todo list and favorite list
+    const [todoList, setTodoList] = useState([{ title: 'My Todo List' }])
+    const [favoriteList, setFavoriteList] = useState([])
+
+    //function to add new item to todo list based on its category
+    const addTodo = (newTodo) => {
+        if (newTodo.category === 'mytodo') {
+            setTodoList((prevTodoList) => [...prevTodoList, newTodo])
+        } else if (newTodo.category === 'favorite') {
+            setFavoriteList((prevFavoritList) => [...prevFavoritList, newTodo])
+        }
+    }
+
     return (
         <div>
             <header>
                 <h1>Todo List</h1>
             </header>
             <hr />
-            <AddTodoForm onAddtodo={setnewTodo} />
-            <p>
-                <strong>New Todo List Item:{newTodo}</strong>{' '}
-            </p>
-            <TodoList />
+            <AddTodoForm onAddtodo={addTodo} />
+            <TodoList todoList={todoList} favoriteList={favoriteList} />
         </div>
     )
 }
